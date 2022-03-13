a45275ad7947d5dd0213e6c86a3f41fe6833a446
BRQ <bnowak4744@gmail.com>
2022-03-13 14:37:49 +0100

    Updated to first version of finished project

--- source.js
+++ source.js
@@ -1,6 +1,17 @@
 let numberInput = document.querySelector('#number-input');
+let decimalNumbers = 1000;
 let selectionStart = null;
 let equation;
+
+document.getElementById('dn-input').addEventListener('change', () =>
+{
+    const inputValue = document.getElementById('dn-input').value;
+    if(inputValue > decimalNumbers.toString().length-1)
+        decimalNumbers *= 10;
+    else    
+        decimalNumbers /= 10;
+    console.log(decimalNumbers)
+});
 
 numberInput.addEventListener('blur', () =>
 {
@@ -52,8 +63,12 @@ function addToInput(key)
 
         case 'x²' :
         {
-
-        }
+            let string = numberInput.value;
+            let index = numberInput.selectionStart;
+            string = string.slice(0, index) + '²' + string.slice(index, string.length);
+            numberInput.value = string;
+            numberInput.setSelectionRange(index+1, index+1);
+        } break;
 
         default: 
         {
@@ -69,8 +84,9 @@ function addToInput(key)
 
 function resolveMainCalculation(inputValue)
 {
-    equation = splitEquations(inputValue);
-    let result = resolveCalculation();
+    equation = inputValue;
+    let equationCalculate = splitEquations(inputValue);
+    let result = resolveCalculation(equationCalculate);
 
     if(!isNaN(result))
     {
@@ -85,16 +101,16 @@ function resolveMainCalculation(inputValue)
 }
 
 
-function resolveCalculation()
+function resolveCalculation(equation)
 {
-    let separatedEquasion = separateEquasion(); 
+    let separatedEquasion = separateEquasion(equation); 
     let result = NaN;
     if(separatedEquasion)
     {
         if(separatedEquasion.numbers.length == 1)
-            result = separatedEquasion.numbers.at(0);
+            result = Math.round((separatedEquasion.numbers.at(0)) * decimalNumbers) / decimalNumbers;
         else
-            result = Math.round((resolve(separatedEquasion.numbers, separatedEquasion.operations) + Number.EPSILON) * 1000) / 1000;
+            result = Math.round((resolve(separatedEquasion.numbers, separatedEquasion.operations) + Number.EPSILON) * decimalNumbers) / decimalNumbers;
     }
     return result;
 }
@@ -107,7 +123,7 @@ function splitEquations(equation)
 
     for(let i = 0; i < equation.length; i++)
     {
-        if(equation.charAt(i) == '(')
+        if(equation.charAt(i) == '('    )
                 first = i;
 
         else if(equation.charAt(i) == ')')
@@ -118,14 +134,16 @@ function splitEquations(equation)
             let result = '';
             if(current.length > 0)
                 result = resolveCalculation(current);
+            let oldLength = equation.length;
             equation = equation.slice(0, first-1) + result + equation.slice(first+current.length+1, equation.length);
+            i -= oldLength - equation.length + 2;
         }
     }
     return equation;
 }
 
 
-function separateEquasion() 
+function separateEquasion(equation) 
 {
     let numbers = [];
     let operations = [];
@@ -137,25 +155,35 @@ function separateEquasion()
 
     for(i; i < equation.length; i++)
     {
-        if((equation.charAt(i-1) != '+' && equation.charAt(i-1) != '÷' && equation.charAt(i-1) != '×' && equation.charAt(i-1) != '-')
-           ||  (equation.charAt(i) == '+' || equation.charAt(i) == '÷' || equation.charAt(i) == '×') )
+        if(   ((equation.charAt(i-1) != '+' && equation.charAt(i-1) != '÷' && equation.charAt(i-1) != '×' && equation.charAt(i-1) != '-')
+              &&  
+              (equation.charAt(i) == '+' || equation.charAt(i) == '÷' || equation.charAt(i) == '×' || equation.charAt(i) == '-'))
+            ||  
+        (equation.charAt(i) == '+' || equation.charAt(i) == '÷' || equation.charAt(i) == '×') )
         {
             if(equation.charAt(i) == '+' || equation.charAt(i) == '÷' || equation.charAt(i) == '×' || equation.charAt(i) == '-')
             {
-                if(first != i)
+                if(first != i && equation.charAt(i-1) != '√') // &&  equation.charAt(i-2) != '√'
                 {
-                    numbers.push(equation.slice(first, i));
+                    numbers.push(isCorrectNumber(equation.slice(first, i)));
                     if(isNaN(numbers[numbers.length-1]))
                         return NaN;
                     first = i+1;
                 }
-               
-                operations.push(equation.slice(i, i+1));
+                if(equation.charAt(i) == '-' &&equation.charAt(i+1) == '-')
+                {
+                    //equation = equation.slice(i+1, i+2)
+                    operations.push('+');
+                    first++;
+                    i++;
+                }
+                else
+                    operations.push(equation.slice(i, i+1));
             }
         }
         else if(i+1 == equation.length)
         {
-            numbers.push(equation.slice(first, i+1));
+            numbers.push(isCorrectNumber(equation.slice(first, i+1)));
             if(isNaN(numbers[numbers.length-1]))
                 return NaN;
             break;
@@ -214,6 +242,49 @@ function resolve(numbers, operations)
         return NaN;
     return numbers[0];
 }
+
+function isCorrectNumber(number)
+{
+    let i = 0;
+    if(isNaN(number))
+    {
+        let power = false;
+        let precent = false;
+        if(number.charAt(i) == '-' && number.charAt(i+1) == '-')
+            number = number.slice(i+2, number.length);
+        else if(number.charAt(i) == '-')
+            i++;
+        if(number.charAt(number.length-1) == '%')    
+        {
+            number = number.slice(0, number.length-1);
+            precent = true;
+        }
+        if(number.charAt(number.length-1) == '²')
+        {
+            number = number.slice(0, number.length-1);
+            power = true;
+        }
+        if(number.charAt(i) == '√')
+        {
+            number = number.slice(0, i) + number.slice(i+1, number.length);
+            if(number.charAt(i) == '-' && number.charAt(i+1) == '-')
+                number = number.slice(i+2, number.length);
+            if(!isNaN(number) && number.slice(i, number.length) < 0)
+            {
+                generateAlert('Your input is wrong, there is no square root from negative number', document.querySelector('.number-io'), document.querySelector('#number-input'));
+                return NaN;
+            }
+            else   
+                number = number.slice(0, i) + Math.sqrt(number.slice(i, number.length))
+        }
+        if(power == true)
+            number = Math.pow(number, 2);
+        if(precent == true)
+            number /= 100;
+    }
+    return number;
+}
+
 
 let resultContentChildrenW = 0;
 function saveResult(calculation, result) 
